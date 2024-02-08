7343e9d4e25323ac6a37c63eeba2988c1f75cc6b
Aryan Kumar <81426042+CRGaming78@users.noreply.github.com>
2024-02-09 00:14:51 +0530

    new programs

---
+++ Function/Circle area.py
@@ -0,0 +1,8 @@
+def calculate_circle_area(radius):
+    pi_approx = 3.14159
+    area = pi_approx*(radius**2)
+    return area
+
+radius = float(input("Enter the radius of the circle: "))
+area = calculate_circle_area(radius)
+print(f"The area of the circle with radius {radius} is: {area:.2f}")
