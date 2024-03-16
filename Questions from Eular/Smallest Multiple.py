0cbba001aac8dc419fbd86e7d0defbda46dbdb8e
Aryan Kumar <81426042+CRGaming78@users.noreply.github.com>
2024-03-16 10:20:05 +0530

    New program

---
+++ Questions from Eular/Smallest Multiple.py
@@ -0,0 +1,22 @@
+#We have used euclid Algorithm to find the lcm also called smallest multiple
+#* This method much more efficient
+def gcd(n1,n2):
+	remainder=1
+	while remainder!=0:
+		remainder=n1%n2
+		n1=n2
+		n2=remainder
+	return n1
+
+#? lcm of two numbers using euclid Algorithm
+#* lcm=(number1*number2)/GCD(number1,number2)
+def lcm(n1,n2):
+	return (n1*n2)/gcd(n1,n2)
+
+l = lcm(2,3) # loading lcm with starting value
+#? lcm of three numbers n1,n2,n3 is
+#* lcm(lcm(n1,n2),n3)
+for i in range(4,21):
+	l = lcm(l,i)
+
+print ("LCM:",l)
